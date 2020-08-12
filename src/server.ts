7c9a744dfa448ea55ccbfcc2b465af0b74584977
Anna Dunajeva <anna.dunajeva@gmail.com>
2020-08-12 17:19:04 +0300

    remove wrong comment

--- src/server.ts
+++ src/server.ts
@@ -73,7 +73,7 @@ app.use(csrfProtection)
 
 app.use(function (req, res, next) { //new token on each request BREACH attack protection
     res.cookie(process.env.CSRF_COOKIE_KEY || 'XSRF-TOKEN', req.csrfToken(), {
-      sameSite: 'strict', //samesite true does not allow to access cookie in firefox
+      sameSite: 'strict', 
       secure: process.env.ENV === 'production' ? true : false
     })
     next()
